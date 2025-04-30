3d44c4e829516fc8571353dfdce61dda15735fb5
Вика <vika@MacBook-Air-Saan.local>
2025-04-30 21:02:20 +0300

    second

--- collect_files.sh
+++ collect_files.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-inputdir="$1"
-outputdir="$2"
-
-if [ "$#" -ne 2 ]; then
-	echo "Ошибка: не 2 директории"
-	exit 1 
-fi
-
 for file in $(find "$1" -type f); do
     filename=$(basename "$file")
-    dest_file="$2/$filename"
-    if [ -f "$dest_file" ]; then
+    if [ -f "$2/$filename" ]; then
         i=1
-        while [ -f "$dest_file" ]; do
-            dest_file="$2/${filename%.*}_$i.${filename##*.}"
+        name=${filename%.*}
+        ext=${filename##*.}
+        while [ -f "$2/$filename" ]; do
+            filename="$name_$i.$ext"
             ((i++))
         done
     fi
-    cp "$file" "$dest_file"
+    cp "$file" "$2/$filename"
 done
+
+
+
